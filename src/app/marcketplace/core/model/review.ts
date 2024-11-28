a1d45dfb939a3de844dce13df33b781b724305c2
Mohamed Elaboudi <mohamedelaboudi2@gmail.com>
2024-11-28 14:34:45 +0100

    Save befor swithch

---
+++ src/app/marcketplace/core/model/review.ts
@@ -0,0 +1,6 @@
+export class Review { 
+
+    reviewerName?:string;
+    rating?:number;
+    comment?: string;
+}
--- src/app/marcketplace/pages/home/product-deatils/product-info/product-info.component.ts
+++ src/app/marcketplace/pages/home/product-deatils/product-info/product-info.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-info',
@@ -7,6 +8,25 @@ import { Component } from '@angular/core';
   templateUrl: './product-info.component.html',
   styleUrl: './product-info.component.scss'
 })
-export class ProductInfoComponent {
+export class ProductInfoComponent implements OnInit {
+
+  productId!: number;
+  product!: any; // Add this property
+
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.productId = Number(this.route.snapshot.paramMap.get('id'));
+  
+    // Fetch product details (for now, simulate with hardcoded data)
+    const allProducts = [
+      { id: 1, name: 'Basic Tee', description: 'Black', price: 35.0 },
+      { id: 2, name: 'Advanced Tee', description: 'White', price: 40.0 },
+      { id: 3, name: 'Premium Hoodie', description: 'Gray', price: 75.0 },
+      { id: 4, name: 'Stylish Hat', description: 'Beige', price: 25.0 },
+    ];
+  
+    this.product = allProducts.find((p) => p.id === this.productId);
+    console.log('Product Details:', this.product);
+  }
 
 }
